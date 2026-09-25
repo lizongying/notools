6793e42d36c82e06b1fc0146f89bc65d913f3352
李宗英 <lizongying@msn.com>
2026-09-25 11:19:08 +0800

    ci(release): add history.sh pipeline and include HISTORY.md in release body

---
+++ history.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+VERSION="$1"
+TARGET_HEADER="## ${VERSION}"
+in_changelog=false
+
+while IFS= read -r line; do
+    if [[ "$line" == "# "* ]]; then
+        continue
+    fi
+
+    if [[ $in_changelog == false ]] && [[ "$line" == "${TARGET_HEADER}"* ]]; then
+        in_changelog=true
+        continue
+    fi
+
+    if [[ $in_changelog == true ]] && [[ "$line" == "## "* ]]; then
+        break
+    fi
+
+    if [[ $in_changelog == true ]]; then
+        echo "$line"
+    fi
+done < HISTORY.md
